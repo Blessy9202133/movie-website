bf08c5113afb9ff66852d841af3157b9e726fb9c
Blessy_123 <122211283+Blessy9202133@users.noreply.github.com>
2024-06-11 13:37:56 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+//import React, { useContext } from 'react'
+// import { AppContext } from './context'
+//import { useGlobalContext } from './context'
+import Search from "./Search";
+import Movies from "./Movies";
+
+
+const Home = () => {
+    //const name = useContext(AppContext)
+   // const name = useGlobalContext();
+  return (
+    <>
+
+    <Search />
+    <Movies />
+    
+    </>
+    
+  );
+}
+
+export default Home
